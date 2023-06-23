e37f64da41173f95e674c2dd963fb3a211f6b3f7
Jules <juliaghany@msn.com>
2023-06-23 12:10:09 -0500

    fix typo

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-  link: authLink.contact(httpLink),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
